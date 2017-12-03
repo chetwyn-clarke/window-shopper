6c971cb1a7ab1f58d9f0b444fe192467f9445ea2
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-12-03 18:57:25 -0500

    Finished lesson 01

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,40 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Chetwyn on 12/3/17.
+//  Copyright © 2017 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        //Setting the placeholder properties, remembering that the placeholder might be empty.
+        
+        /*
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+         */
+        
+        //Better solution
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
